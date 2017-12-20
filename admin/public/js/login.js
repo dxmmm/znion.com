0b9f478bc010cfeb866e7ff3db3d4947209b2cc1
BeenHot <wanmin@beenhot.com>
2017-12-20 21:53:27 +0800

    给登录界面加上bootstrap提供的输入框校准，模态框加载效果

---
+++ admin/public/js/login.js
@@ -0,0 +1,35 @@
+$(function() {
+	$("#username,#password").blur(function() {
+		$(this).parent().removeClass("has-error has-feedback has-success");
+		$(this).parent().find("span.form-control-feedback").remove();
+		var inputLen = $(this).val().length;
+		var errorDom = "<span class='glyphicon glyphicon-remove form-control-feedback'></span>";
+		var rightDom = "<span class='glyphicon glyphicon-ok form-control-feedback'></span>";
+		if(inputLen < 3 || inputLen > 5) {
+			$(this).parent().addClass("has-error has-feedback").append(errorDom);
+		}else {
+			$(this).parent().addClass("has-success has-feedback").append(rightDom);
+		}
+	});
+	$("#btn-login").click(function() {
+		var username = $("#username").val();
+		var password = $("#password").val();
+		if(username != "admin" && password != "123") {
+			var errorTip = "<div class='alert alert-danger alert-dismissible text-center'>";
+			errorTip += "<button class='close' data-dismiss='alert'>";
+			errorTip += "<span>&times;</span>";
+			errorTip += "</button>";
+			errorTip += "</button>";
+			errorTip += "用户名或者密码错误";
+			errorTip += "</div>";
+			$("#login").append(errorTip);
+		}else {
+			$('#myModal').modal("show");
+			setTimeout(function() {
+				$('#myModal').modal("hide");
+				sessionStorage.setItem("admin_user","admin");
+				window.location.href = "main.html";
+			},3000);
+		}
+	});
+})
